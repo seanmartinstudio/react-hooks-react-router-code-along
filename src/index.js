df3e04a5d95f6beaefb9be70301c0a7e118bb6b3
Sean Martin <sean@seanmartinstudio.com>
2022-03-22 15:03:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,112 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { NavLink } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-function Home() {
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink to= "/" 
+      exact 
+      style={linkStyles} 
+      activeStyle={{
+        background: "darkblue",
+        }}
+      >
+        Home
+        </NavLink>
+        <NavLink
+        to= "/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+      to= "/login"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: "darkblue",
+      }}
+    >
+      Login
+    </NavLink>
+    </div>
+  )
+}
+
+function Home() {
+  return <h1>Home!</h1>;
+}
+
+
+function About() {
+  return <h1>This is my about component!</h1>;
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+document.getElementById("root"));
